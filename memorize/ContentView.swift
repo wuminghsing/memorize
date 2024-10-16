31888bf1153e9cc0f427807569a098e6df574e6d
wuminghsing <wums@nkps.tp.edu.tw>
2024-10-16 11:52:20 +0800

    Initial Commit

---
+++ memorize/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  memorize
+//
+//  Created by 吳明行 on 2024/10/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, nkps!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+ 
---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by 吳明行 on 2024/10/16.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
